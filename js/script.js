e052535c8adf0dd8974330b496236ec3f3d47111
Youssef Hany <youssefnagdy@gmail.com>
2020-06-28 04:44:09 +0200

    Add Effect To Navbar on Scroll

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
 /**
  * Created by YoussefNagdy on 6/26/2020.
  */
+
+const navbar = document.getElementById('navbar');
+let scrolled = false;
+window.onscroll = function () {
+  if(window.pageYOffset > 100)
+  {
+      navbar.classList.remove('top');
+      if(!scrolled)
+      {
+            navbar.style.transform = "translateY(-70px)";
+            console.log(scrolled);
+      }
+      setTimeout(function () {
+          navbar.style.transform = "translateY(0)";
+          scrolled = true;
+      },200);
+  }else{
+      navbar.classList.add('top');
+      scrolled = false;
+  }
+};
+
+
